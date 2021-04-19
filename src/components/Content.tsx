94248e46bacf3e65718157367d9fcfbc6ed35dfd
Rafael Ferreira <ffael@live.com>
2021-04-18 22:19:01 -0400

    Creates a component to handle main content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,38 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from './MovieCard'
+interface ContentProp {
+  selectedGenre: {
+    title: string
+  },
+  movies: Array<
+    {
+      imdbID: string,
+      Title: string,
+      Poster: string,
+      Runtime: string,
+      Ratings: Array<{
+        Source: string;
+        Value: string;
+      }>
+    }>
+}
+
+
+
+export function Content({ selectedGenre, movies }: ContentProp) {
+  return (
+    <>
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </>
+  )
+
 }
